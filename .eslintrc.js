aa12bd8140eba6b0e824fbe23c8364d027696d4b
Miroslav Martynov <marty.mircha@gmail.com>
2019-08-10 17:25:42 +0200

    chore: eslint routine

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,9 +13,16 @@ module.exports = {
     "sourceType": "module"
   },
   "rules": {
+    "curly": "error",
     "indent": [
       "error",
       2
+    ],
+    "padding-line-between-statements": [
+      "error",
+      { blankLine: "always", prev: "*", next: "return" },
+      { blankLine: "always", prev: ["const", "let", "var"], next: "*"},
+      { blankLine: "any",    prev: ["const", "let", "var"], next: ["const", "let", "var"]}
     ],
     "linebreak-style": [
       "error",
--- src/getAllColors.js
+++ src/getAllColors.js
@@ -1,8 +1,8 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const path = require('path');
-const colorRegexp = /(#[A-F\d]{3}\b|#[A-F\d]{6}\b)|(rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?([, \.\d]+)?\))/gi;
+const fs = require("fs");
+const path = require("path");
+const colorRegexp = /(#[A-F\d]{3}\b|#[A-F\d]{6}\b)|(rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?([, \.\d]+)?\))/gi; // eslint-disable-line
 const sassVariableRegexp = /(\$[\S\d]+)\b/gi;
 let colorMap;
 let fileCounter = 0;
@@ -16,10 +16,11 @@ let fileCounter = 0;
  * @return {[type]}          Array of colors
  */
 function parseStylesheetsColors(data, filePath, map) {
-  const lines = data.split('\n');
+  const lines = data.split("\n");
   let result = [];
   let test;
   let totalReadData = 0;
+
   for (let i = 0, len = lines.length; i < len; i++) {
     let lineStr = `${filePath}:${i+1}:`;
 
@@ -34,11 +35,13 @@ function parseStylesheetsColors(data, filePath, map) {
         startPos: totalReadData - (lines[i].length - test.index) - 1,
         xPath: lineStr + (test.index + 1),
       };
+
       addToMap(test[0], colorData, map);
       result.push(test[0]);
     }
   }
   console.log(`totalReadData: ${totalReadData}, data.length: ${data.length}`);
+
   return result;
 }
 
@@ -52,7 +55,8 @@ function parseColorSheme(data) {
   var result = [];
   var test;
   var variable;
-  var lines = data.split('\n');
+  var lines = data.split("\n");
+
   for (var i = 0, len = lines.length; i < len; i++) {
     // FIXME: Find out how to extract alpha channel
     while (test = colorRegexp.exec(lines[i])) {
@@ -64,6 +68,7 @@ function parseColorSheme(data) {
       }
     }
   }
+
   return result;
 }
 
@@ -71,8 +76,10 @@ function parseColorSheme(data) {
 function addToMap(color, colorData, map) {
   var normalizedColor = color.toLowerCase();
   var longColor = convertShortHEXtoLong(normalizedColor);
+
   if (map.has(longColor)) {
     let val = map.get(longColor);
+
     val.meta.push(colorData);
     val.index = getColorIndex(longColor);
     map.set(longColor, val);
@@ -81,26 +88,35 @@ function addToMap(color, colorData, map) {
       meta: [],
       index: 0
     };
+
     val2.meta.push(colorData);
     val2.index = getColorIndex(longColor);
     map.set(longColor, val2);
   }
+
   return map;
 }
 
 function pathType(path) {
   var stat = fs.statSync(path);
   var type;
-  if (stat.isFile()) type = 'FILE';
-  else if (stat.isDirectory()) type = 'DIRECTORY';
-  else type = undefined;
+
+  if (stat.isFile()) {
+    type = "FILE";
+  } else if (stat.isDirectory()) {
+    type = "DIRECTORY";
+  } else {
+    type = undefined;
+  }
+
   return type;
 }
 
 function processDir(path, skip) {
-  var files = [];
-  if (typeof path === 'array') {
-    for (var el of path) {
+  let files = [];
+
+  if (Array.isArray(path)) {
+    for (let el of path) {
       files = fs.readdirSync(el);
       main(files, el, skip);
     }
@@ -116,13 +132,13 @@ function main(files, dir, skip) {
     let filePath = path.resolve(dir, file);
     let pType = pathType(filePath);
 
-    if (pType === 'FILE') {
+    if (pType === "FILE") {
       if (filePath === skip) {
         continue;
       }
 
       processFile(filePath);
-    } else if (pType === 'DIRECTORY') {
+    } else if (pType === "DIRECTORY") {
       processDir(filePath, skip);
     } else {
       console.log(`${filePath} is not file`);
@@ -132,20 +148,23 @@ function main(files, dir, skip) {
 }
 
 function processFile(filePath) {
-  let data = fs.readFileSync(filePath, 'utf-8');
+  let data = fs.readFileSync(filePath, "utf-8");
   let colors = parseStylesheetsColors(data, filePath, colorMap);
+
   countAndPrintProcessedFiles(filePath, colors);
 }
 
 function handleScheme(filePath) {
-  var data = fs.readFileSync(filePath, 'utf-8');
+  var data = fs.readFileSync(filePath, "utf-8");
   var scheme = parseColorSheme(data);
+
   return scheme;
 }
 
 function countAndPrintProcessedFiles(filePath, colors) {
   ++fileCounter;
   console.log(`${fileCounter}: ${filePath} — found ${colors.length} colors`);
+
   return fileCounter;
 }
 
@@ -156,7 +175,7 @@ function mainHandler(dir, skip) {
   var end, diff;
   let dirType = pathType(dir);
 
-  if (dirType === 'FILE') {
+  if (dirType === "FILE") {
     processFile(dir);
   } else {
     processDir(dir, skip);
@@ -165,6 +184,7 @@ function mainHandler(dir, skip) {
   diff = end - start;
   console.log(`Finished for ${diff}ms, found ${colorMap.size} colors`);
   fileCounter = 0;
+
   return colorMap;
 }
 
@@ -174,14 +194,15 @@ function mainHandler(dir, skip) {
  * @returns {string}
  */
 function generateMarkup(map) {
-  var colors = '';
+  // var colors = "";
   var sortedColors = insertionSortForColors([...map.keys()], map);
-  var html = '';
+  var html = "";
 
   sortedColors.forEach((val)=>{
-    let title = '';
+    let title = "";
     let index = map.get(val).index;
     let appearsCounter = 0;
+
     map.get(val).meta.forEach((meta)=>{
       appearsCounter++;
       title += `${meta.xPath}\n`;
@@ -197,28 +218,32 @@ function generateMarkup(map) {
       </div>
     `;
   });
+
   return html;
 }
 
 
 function convertShortHEXtoLong(hex) {
-  var tmp = '';
+  var tmp = "";
+
   if (hex && hex.length === 4) {
     tmp = hex[0] + hex[1] + hex[1] + hex[2] + hex[2] + hex[3] + hex[3];
   } else if (!hex) {
-    return '#000000';
+    return "#000000";
   } else {
     tmp = hex;
   }
+
   return tmp;
 }
 
 function rgb2hex(rgb){
- rgb = rgb.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
- return (rgb && rgb.length === 4) ? "#" +
+  rgb = rgb.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
+
+  return (rgb && rgb.length === 4) ? "#" +
   ("0" + parseInt(rgb[1],10).toString(16)).slice(-2) +
   ("0" + parseInt(rgb[2],10).toString(16)).slice(-2) +
-  ("0" + parseInt(rgb[3],10).toString(16)).slice(-2) : '';
+  ("0" + parseInt(rgb[3],10).toString(16)).slice(-2) : "";
 }
 
 
@@ -236,6 +261,7 @@ function compareTwoColorIndex( colorA, colorB ) {
   // console.log(`arA = ${arA}; arB = ${arB}`);
   var indexA = colorMap.get(colorA);
   var indexB = colorMap.get(colorB);
+
   if (indexA && indexB) {
     return (indexA.index - indexB.index) < 0;
   } else {
@@ -251,6 +277,7 @@ function compareTwoColorIndex( colorA, colorB ) {
  */
 function swap( arr, posA, posB ) {
   var temp = arr[posA];
+
   arr[posA] = arr[posB];
   arr[posB] = temp;
 }
@@ -266,11 +293,13 @@ function insertionSort( array ) {
 
   for (var i = 1; i <= arr.length; i++) {
     var currentPos = i;
+
     while (currentPos > 0 && compareTwoNumbers(parseInt(arr[currentPos - 1]), parseInt(arr[currentPos]))) {
       swap(arr, currentPos, currentPos - 1);
       currentPos -= 1;
     }
   }
+
   return arr;
 }
 
@@ -280,17 +309,19 @@ function insertionSort( array ) {
  * @param arr
  * @returns {Array|number}
  */
-function insertionSortForColors( array, map ) {
+function insertionSortForColors( array ) {
   // console.log(array);
-    var arr = array.slice();
+  var arr = array.slice();
 
   for (var i = 1; i <= arr.length; i++) {
     var currentPos = i;
+
     while (currentPos > 0 && compareTwoColorIndex(arr[currentPos - 1], arr[currentPos])) {
       swap(arr, currentPos, currentPos - 1);
       currentPos -=1;
     }
   }
+
   return arr;
 }
 
@@ -298,7 +329,8 @@ function insertionSortForColors( array, map ) {
 
 function getColorIndex(color) {
   var index, hex;
-  if (color && color[0] === '#') {
+
+  if (color && color[0] === "#") {
     index = getHEXValue(color);
   } else if (!color) {
     return 0;
@@ -306,12 +338,14 @@ function getColorIndex(color) {
     hex = rgb2hex(color);
     index = getHEXValue(hex);
   }
+
   return index;
 }
 
 function getHEXValue(hex) {
   var bytes = hex.slice(1);
   var value = parseInt(bytes, 16);
+
   return isNaN(value) ? 0 : value;
 }
 
